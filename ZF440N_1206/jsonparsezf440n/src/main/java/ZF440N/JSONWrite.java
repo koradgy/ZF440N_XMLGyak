1eb51bbcb355de6cfa97afd4829698d8874c7c44
unknown <korad.gy@gmail.com>
2023-12-12 09:54:27 +0100

    json write change

--- ZF440N_1206/jsonparsezf440n/src/main/java/ZF440N/JSONWrite.java
+++ ZF440N_1206/jsonparsezf440n/src/main/java/ZF440N/JSONWrite.java
@@ -1,32 +1,67 @@
 package ZF440N;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class JSONWrite {
     public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
+        try {
+            JSONParser parser = new JSONParser();
 
-        try (FileReader reader = new FileReader("kurzusfelvetelZF440N.json")) {
-            Object obj = parser.parse(reader);
-            File file = new File("kurzusfelvetelZF440N_1.json");
+            Object obj = parser.parse(new FileReader("kurzusfelvetelZF440N.json"));
+
             JSONObject jsonObject = (JSONObject) obj;
-            OutputStreamWriter wr = new OutputStreamWriter(System.out, StandardCharsets.UTF_8);
-            wr.write(jsonObject.toString());
-            wr.flush();
-            PrintWriter pw = new PrintWriter(file);
-            pw.println(jsonObject.toString());
-            pw.close();
-        } catch (IOException | ParseException e) {
+
+            displayJsonObject("", jsonObject);
+
+            writeJsonToFile(jsonObject, "kurzusfelvetelZF440N1.json");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayJsonObject(String prefix, JSONObject jsonObject) {
+        for (Object key : jsonObject.keySet()) {
+            String keyStr = (String) key;
+            Object value = jsonObject.get(keyStr);
+
+            if (value instanceof JSONObject) {
+                System.out.println(prefix + keyStr + ": ");
+                displayJsonObject(prefix + "  ", (JSONObject) value);
+            } else if (value instanceof JSONArray) {
+                System.out.println(prefix + keyStr + ": ");
+                displayJsonArray(prefix + "  ", (JSONArray) value);
+            } else {
+                System.out.println(prefix + keyStr + ": " + value);
+            }
+        }
+    }
+
+    private static void displayJsonArray(String prefix, JSONArray jsonArray) {
+        for (Object item : jsonArray) {
+            if (item instanceof JSONObject) {
+                displayJsonObject(prefix + "  ", (JSONObject) item);
+                System.out.println();
+            } else if (item instanceof JSONArray) {
+                displayJsonArray(prefix + "  ", (JSONArray) item);
+            } else {
+                System.out.println(prefix + "  " + item);
+            }
+        }
+    }
+
+    private static void writeJsonToFile(JSONObject jsonObject, String filePath) {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            jsonObject.writeJSONString(fileWriter);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
